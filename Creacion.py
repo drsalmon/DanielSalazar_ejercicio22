e508f94db142bf688f0148b8b71685637304e6a5
drsalmon <46974493+drsalmon@users.noreply.github.com>
2019-04-04 14:24:43 -0500

    Update Creacion.py

--- Creacion.py
+++ Creacion.py
@@ -3,13 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-a = os.system("g++ Ejercicio.cpp -o Ejercicio.x")
-a = os.system("./Ejercicio.x > Ejercicio.dat")
+B = os.system("g++ Ejercicio.cpp -o Ejercicio.x")
+B = os.system("./Ejercicio.x > Ejercicio.dat")
 
 plt.figure()
 datos = np.loadtxt("Ejercicio.dat")
-plt.hist(datos[0:], bins=70)
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.hist(datos[0:], bins=70, density=False)
+plt.xlabel('Valor de X')
+plt.title('Histrograma Metopolis Hastings')
+plt.ylabel('# Iteraciones')
+plt.grid()
 plt.savefig("Histograma.png")
+
 
